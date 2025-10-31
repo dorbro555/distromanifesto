5a4d7af60e591a3a3fe59806a9b3acb8bc391741
dorbro555 <dorbro555@proton.me>
2025-10-31 14:08:49 -0700

    removed obselete wizard.rs

--- cli/src/lib.rs
+++ cli/src/lib.rs
@@ -1,10 +1,8 @@
 pub mod setup;
 pub mod manifest;
-pub mod wizard;
 pub mod verify;
 // pub mod utils;
 
 pub use setup::ensure_hidden_dir;
 // pub use manifest::Manifest;
-pub use wizard::launch_wizard;
 pub use verify::verify_manifest;
